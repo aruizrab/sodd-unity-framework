fe6d8445bfe22376338ecf77d07269aed238aede
aruizrab <aruizrab@users.noreply.github.com>
2024-02-26 20:13:32 +0100

    feat(events): add IEvent interface
    This commit adds a new IEvent interface in the sodd-unity-framework package. This interface represents an event that can be listened to and invoked with a type of payload. It provides methods for adding and removing listeners as well as invoking the event with a specific payload.
    
    Signed-off-by: Alex Ruiz <68024691+aruizrab@users.noreply.github.com>

---
+++ Runtime/Events/IEvent.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace SODD.Events
+{
+    /// <summary>
+    ///     Represents an event that can be listened to and invoked.
+    /// </summary>
+    /// <typeparam name="T">The type of the event payload.</typeparam>
+    public interface IEvent<T>
+    {
+        /// <summary>
+        ///     Adds a listener to the event.
+        /// </summary>
+        /// <typeparam name="T">The type of the payload.</typeparam>
+        /// <param name="listener">The listener to add.</param>
+        void AddListener(Action<T> listener);
+
+        /// <summary>
+        ///     Removes the specified listener from the event.
+        /// </summary>
+        /// <typeparam name="T">The type of the listener's argument.</typeparam>
+        /// <param name="listener">The listener to be removed.</param>
+        void RemoveListener(Action<T> listener);
+
+        /// <summary>
+        ///     Invokes the event and notifies all registered listeners with the specified payload.
+        /// </summary>
+        /// <typeparam name="T">The type of the payload.</typeparam>
+        /// <param name="payload">The payload to pass to the listeners.</param>
+        void Invoke(T payload);
+    }
+}
