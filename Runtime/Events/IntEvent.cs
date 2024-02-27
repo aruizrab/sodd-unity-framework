e8cea717cb3d1fba7c9e1e022139ce928fd525ea
Alex Ruiz <68024691+aruizrab@users.noreply.github.com>
2024-02-27 19:41:32 +0100

    feat(events): add IntEvent
    - Implement IntEvent class for integer-based events, extending the abstract Event<T> class.
    - Add NUnit tests for IntEvent to ensure correct listener registration, event invocation, and listener removal.
    - Add editor script for IntEvent class to trigger IntEvents from the inspector for a better debuggability.
    
    Signed-off-by: Alex Ruiz <68024691+aruizrab@users.noreply.github.com>

---
+++ Runtime/Events/IntEvent.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace SODD.Events
+{
+    /// <summary>
+    ///     A ScriptableObject that represents an event carrying an integer payload.
+    /// </summary>
+    /// <seealso cref="Event{T}" />
+    [CreateAssetMenu(menuName = Framework.Events.Int, fileName = nameof(IntEvent), order = Framework.MenuOrders.Int)]
+    public sealed class IntEvent : Event<int>
+    {
+    }
+}
