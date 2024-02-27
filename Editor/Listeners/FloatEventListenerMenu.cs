c967cfec797eb0f60d583ed15a7ba3f3731d1405
Alex Ruiz <a.ruiz.rabasseda@gmail.com>
2024-02-28 00:17:43 +0100

    feat(listeners): add FloatEventListener
    The commit introduces a FloatEventListener in the SODD Unity framework under the listeners directory. It also adds the corresponding menu item for the FloatEventListener in the Unity Editor, so users can easily create FloatEventListener game objects when needed.
    
    Signed-off-by: Alex Ruiz <a.ruiz.rabasseda@gmail.com>

---
+++ Editor/Listeners/FloatEventListenerMenu.cs
@@ -0,0 +1,17 @@
+﻿using SODD.Editor.Utils;
+using SODD.Listeners;
+using UnityEditor;
+using UnityEngine;
+
+namespace SODD.Editor.Listeners
+{
+    public static class FloatEventListenerMenu
+    {
+        [MenuItem("GameObject/" + Framework.EventListeners.Float, priority = Framework.MenuOrders.Float)]
+        [MenuItem("Tools/" + Framework.EventListeners.Float, priority = Framework.MenuOrders.Float)]
+        public static void CreateFloatListener(MenuCommand command)
+        {
+            EditorHelper.CreateGameObject<FloatEventListener>(command.context as GameObject);
+        }
+    }
+}
