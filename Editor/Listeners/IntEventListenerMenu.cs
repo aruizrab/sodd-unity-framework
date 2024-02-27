e80bf4862432a790ff381eb9a17a0e4fed537f63
Alex Ruiz <a.ruiz.rabasseda@gmail.com>
2024-02-28 00:11:05 +0100

    feat(listeners): add IntEventListener
    This commit introduces a new event listener dedicated to integer events for the SODD Unity Framework. It also includes the associated menu items for the listener, facilitating rapid addition of this component while in the Unity editor.
    
    Signed-off-by: Alex Ruiz <a.ruiz.rabasseda@gmail.com>

---
+++ Editor/Listeners/IntEventListenerMenu.cs
@@ -0,0 +1,17 @@
+﻿using SODD.Editor.Utils;
+using SODD.Listeners;
+using UnityEditor;
+using UnityEngine;
+
+namespace SODD.Editor.Listeners
+{
+    public static class IntEventListenerMenu
+    {
+        [MenuItem("GameObject/" + Framework.EventListeners.Int, priority = Framework.MenuOrders.Int)]
+        [MenuItem("Tools/" + Framework.EventListeners.Int, priority = Framework.MenuOrders.Int)]
+        public static void CreateIntListener(MenuCommand command)
+        {
+            EditorHelper.CreateGameObject<IntEventListener>(command.context as GameObject);
+        }
+    }
+}
