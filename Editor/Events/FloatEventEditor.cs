466d076416abb17878a6edca54d8d46ae2309d7d
Alex Ruiz <a.ruiz.rabasseda@gmail.com>
2024-02-27 22:36:32 +0100

    feat(events): add FloatEvent
    - Implement FloatEvent class extending Event<float> to handle floating-point event payloads.
    - Add NUnit tests for FloatEvent to verify listener registration, event invocation, and listener removal functionality.
    - Ensure FloatEvent integrates seamlessly with the existing event system, enhancing the flexibility for events with float payloads.
    
    Signed-off-by: Alex Ruiz <a.ruiz.rabasseda@gmail.com>

---
+++ Editor/Events/FloatEventEditor.cs
@@ -0,0 +1,10 @@
+﻿using SODD.Events;
+using UnityEditor;
+
+namespace SODD.Editor.Events
+{
+    [CustomEditor(typeof(FloatEvent))]
+    public class FloatEventEditor : EventEditor<float>
+    {
+    }
+}
