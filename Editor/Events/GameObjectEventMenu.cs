43c52fca34bf465bf55a166f4eba0cf96795c707
Alex Ruiz <a.ruiz.rabasseda@gmail.com>
2024-02-28 18:11:26 +0100

    feat(events): add GameObjectEvent
    Signed-off-by: Alex Ruiz <a.ruiz.rabasseda@gmail.com>

---
+++ Editor/Events/GameObjectEventMenu.cs
@@ -0,0 +1,15 @@
+﻿using SODD.Editor.Utils;
+using SODD.Events;
+using UnityEditor;
+
+namespace SODD.Editor.Events
+{
+    public static class GameObjectEventMenu
+    {
+        [MenuItem("Tools/" + Framework.Events.GameObject, priority = Framework.MenuOrders.GameObject)]
+        public static void Create(MenuCommand command)
+        {
+            EditorHelper.CreateScriptableObject<GameObjectEvent>();
+        }
+    }
+}
---
+++ Runtime/Events/GameObjectEvent.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace SODD.Events
+{
+    /// <summary>
+    ///     A ScriptableObject that represents an event carrying an GameObject payload.
+    /// </summary>
+    /// <seealso cref="Event{T}" />
+    [CreateAssetMenu(menuName = Framework.Events.GameObject, fileName = nameof(GameObjectEvent), order = Framework.MenuOrders.GameObject)]
+    public sealed class GameObjectEvent : Event<GameObject>
+    {
+    }
+}
