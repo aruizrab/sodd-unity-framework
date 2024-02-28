115aeeb8a1cdd40124a41618cc48571feebe75b1
Alex Ruiz <a.ruiz.rabasseda@gmail.com>
2024-02-28 18:16:07 +0100

    feat(listeners): add GameObjectEventListener
    Signed-off-by: Alex Ruiz <a.ruiz.rabasseda@gmail.com>

---
+++ Editor/Listeners/GameObjectEventListenerMenu.cs
@@ -0,0 +1,17 @@
+﻿using SODD.Editor.Utils;
+using SODD.Listeners;
+using UnityEditor;
+using UnityEngine;
+
+namespace SODD.Editor.Listeners
+{
+    public static class GameObjectEventListenerMenu
+    {
+        [MenuItem("GameObject/" + Framework.EventListeners.GameObject, priority = Framework.MenuOrders.GameObject)]
+        [MenuItem("Tools/" + Framework.EventListeners.GameObject, priority = Framework.MenuOrders.GameObject)]
+        public static void CreateGameObjectListener(MenuCommand command)
+        {
+            EditorHelper.CreateGameObject<GameObjectEventListener>(command.context as GameObject);
+        }
+    }
+}
