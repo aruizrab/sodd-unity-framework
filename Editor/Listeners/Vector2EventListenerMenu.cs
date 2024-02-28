d077d86911e609ff8cb983d05cb4fdc800981fb4
Alex Ruiz <a.ruiz.rabasseda@gmail.com>
2024-02-28 01:14:19 +0100

    feat(listeners): add Vector2EventListener
    Signed-off-by: Alex Ruiz <a.ruiz.rabasseda@gmail.com>

---
+++ Editor/Listeners/Vector2EventListenerMenu.cs
@@ -0,0 +1,17 @@
+﻿using SODD.Editor.Utils;
+using SODD.Listeners;
+using UnityEditor;
+using UnityEngine;
+
+namespace SODD.Editor.Listeners
+{
+    public static class Vector2EventListenerMenu
+    {
+        [MenuItem("GameObject/" + Framework.EventListeners.Vector2, priority = Framework.MenuOrders.Vector2)]
+        [MenuItem("Tools/" + Framework.EventListeners.Vector2, priority = Framework.MenuOrders.Vector2)]
+        public static void CreateVector2Listener(MenuCommand command)
+        {
+            EditorHelper.CreateGameObject<Vector2EventListener>(command.context as GameObject);
+        }
+    }
+}
---
+++ Runtime/Listeners/Vector2EventListener.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace SODD.Listeners
+{
+    /// <summary>
+    ///     Represents an event listener for Vector2 events.
+    /// </summary>
+    [AddComponentMenu(Framework.EventListeners.Vector2EventListener, Framework.MenuOrders.Vector2)]
+    public class Vector2EventListener : EventListener<Vector2>
+    {
+    }
+}
