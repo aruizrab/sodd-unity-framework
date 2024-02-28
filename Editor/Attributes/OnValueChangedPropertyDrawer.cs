5d1960662a12e241d10acf64a07137f9614ed23b
Alex Ruiz <a.ruiz.rabasseda@gmail.com>
2024-02-29 00:04:17 +0100

    feat(attributes): add OnValueChanged attribute
    Introduce the OnValueChangedAttribute, enabling automatic method invocation when specified field values are updated in the Unity Editor. This attribute enhances editor workflows by allowing developers to easily implement custom reactions to property changes, facilitating better data management and validation within custom editor scripts. Includes documentation with usage examples to guide integration into existing projects.
    
    Signed-off-by: Alex Ruiz <a.ruiz.rabasseda@gmail.com>

---
+++ Editor/Attributes/OnValueChangedPropertyDrawer.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+using SODD.Attributes;
+using UnityEditor;
+using UnityEngine;
+
+namespace SODD.Editor.Attributes
+{
+    [CustomPropertyDrawer(typeof(OnValueChangedAttribute))]
+    public class OnValueChangedPropertyDrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            EditorGUI.BeginChangeCheck();
+            EditorGUI.PropertyField(position, property, label);
+
+            if (!EditorGUI.EndChangeCheck()) return;
+
+            var targetObject = property.serializedObject.targetObject;
+            var onValueChangedAttribute = attribute as OnValueChangedAttribute;
+            var methodName = onValueChangedAttribute?.MethodName;
+            var classType = targetObject.GetType();
+            var methodInfo = classType.GetMethods().FirstOrDefault(info => info.Name == methodName);
+
+            property.serializedObject.ApplyModifiedProperties();
+
+            if (methodInfo != null && !methodInfo.GetParameters().Any()) methodInfo.Invoke(targetObject, null);
+        }
+    }
+}
---
+++ Runtime/Attributes/OnValueChangedAttribute.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using UnityEngine;
+
+namespace SODD.Attributes
+{
+    /// <summary>
+    ///     Calls the specified method whenever the value of the serialized field is modified in the editor.
+    /// </summary>
+    /// <remarks>
+    ///     The specified method should exist in the same class as the field and should have no parameters.
+    /// </remarks>
+    /// <example>
+    ///     <code>
+    ///         using UnityEngine;
+    ///         using SODD.Attributes;
+    /// 
+    ///         public class ExampleBehaviour : MonoBehaviour
+    ///         {
+    ///             [OnValueChanged("OnHealthChanged")]
+    ///             public int health;
+    /// 
+    ///             private void OnHealthChanged()
+    ///             {
+    ///                 Debug.Log("Health value changed to: " + health);
+    ///             }
+    ///         }
+    ///     </code>
+    /// </example>
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field)]
+    public class OnValueChangedAttribute : PropertyAttribute
+    {
+        public readonly string MethodName;
+
+        public OnValueChangedAttribute(string methodName)
+        {
+            MethodName = methodName;
+        }
+    }
+}
