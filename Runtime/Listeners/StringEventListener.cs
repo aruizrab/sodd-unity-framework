28fd932bcb6f28d7245b55ab925a8edcc34fe033
Alex Ruiz <a.ruiz.rabasseda@gmail.com>
2024-02-28 01:05:00 +0100

    feat(events): add StringEventListener
    Signed-off-by: Alex Ruiz <a.ruiz.rabasseda@gmail.com>

---
+++ Runtime/Listeners/StringEventListener.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace SODD.Listeners
+{
+    /// <summary>
+    ///     Represents an event listener for string events.
+    /// </summary>
+    [AddComponentMenu(Framework.EventListeners.StringEventListener, Framework.MenuOrders.String)]
+    public class StringEventListener : EventListener<string>
+    {
+    }
+}
