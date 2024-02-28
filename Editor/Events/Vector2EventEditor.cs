0c73d3f9a253d9d0bd61699ca3aaab6851799e1e
Alex Ruiz <a.ruiz.rabasseda@gmail.com>
2024-02-28 01:12:59 +0100

    feat(events): add Vector2Event
    Signed-off-by: Alex Ruiz <a.ruiz.rabasseda@gmail.com>

---
+++ Editor/Events/Vector2EventEditor.cs
@@ -0,0 +1,11 @@
+﻿using SODD.Events;
+using UnityEditor;
+using UnityEngine;
+
+namespace SODD.Editor.Events
+{
+    [CustomEditor(typeof(Vector2Event))]
+    public class Vector2EventEditor : EventEditor<Vector2>
+    {
+    }
+}
---
+++ Tests/Runtime/Events/Vector2EventTests.cs
@@ -0,0 +1,63 @@
+﻿using NUnit.Framework;
+using SODD.Events;
+using UnityEngine;
+
+namespace SODD.Editor.Tests.Events
+{
+    public class Vector2EventTests
+    {
+        private Vector2 _receivedValue;
+        private bool _wasCalled;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _receivedValue = Vector2.zero;
+            _wasCalled = false;
+        }
+
+        private void Listener(Vector2 value)
+        {
+            _receivedValue = value;
+            _wasCalled = true;
+        }
+
+        [Test]
+        public void Vector2Event_InvokesListenerWithCorrectValue()
+        {
+            // Arrange
+            var vector2Event = ScriptableObject.CreateInstance<Vector2Event>();
+            var expectedValue = new Vector2(3,5);
+
+            vector2Event.AddListener(Listener);
+
+            // Act
+            vector2Event.Invoke(expectedValue);
+
+            // Assert
+            Assert.IsTrue(_wasCalled, "Listener was not called.");
+            Assert.AreEqual(expectedValue, _receivedValue, "Listener did not receive the correct value.");
+
+            // Cleanup
+            Object.DestroyImmediate(vector2Event);
+        }
+
+        [Test]
+        public void Vector2Event_RemoveListener_StopsReceivingEvents()
+        {
+            // Arrange
+            var vector2Event = ScriptableObject.CreateInstance<Vector2Event>();
+            vector2Event.AddListener(Listener);
+
+            // Act
+            vector2Event.RemoveListener(Listener);
+            vector2Event.Invoke(Vector2.down);
+
+            // Assert
+            Assert.IsFalse(_wasCalled, "Listener was called after being removed.");
+
+            // Cleanup
+            Object.DestroyImmediate(vector2Event);
+        }
+    }
+}
