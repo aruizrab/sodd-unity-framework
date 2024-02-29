dc22ba6aac929e43e81fd38a6476a34e9ca03bbc
Alex Ruiz <a.ruiz.rabasseda@gmail.com>
2024-02-29 18:45:03 +0100

    feat(collections): add ComponentCollection
    Signed-off-by: Alex Ruiz <a.ruiz.rabasseda@gmail.com>

---
+++ Runtime/Collections/ComponentCollection.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace SODD.Collections
+{
+    /// <summary>
+    ///     Represents a collection that stores Components.
+    /// </summary>
+    /// <seealso cref="Collection{T}" />
+    [CreateAssetMenu(menuName = Framework.Collections.Component, fileName = nameof(ComponentCollection),
+        order = Framework.MenuOrders.Component)]
+    public sealed class ComponentCollection : Collection<Component>
+    {
+    }
+}
