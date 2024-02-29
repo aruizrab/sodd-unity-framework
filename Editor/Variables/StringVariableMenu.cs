ab24b07148c36aa51f16ea04861be70c957ed63c
Alex Ruiz <a.ruiz.rabasseda@gmail.com>
2024-02-29 01:12:12 +0100

    feat(variables): add StringVariable
    Signed-off-by: Alex Ruiz <a.ruiz.rabasseda@gmail.com>

---
+++ Editor/Variables/StringVariableMenu.cs
@@ -0,0 +1,15 @@
+﻿using SODD.Editor.Utils;
+using SODD.Variables;
+using UnityEditor;
+
+namespace SODD.Editor.Variables
+{
+    public static class StringVariableMenu
+    {
+        [MenuItem("Tools/" + Framework.Variables.String, priority = Framework.MenuOrders.String)]
+        public static void Create(MenuCommand command)
+        {
+            EditorHelper.CreateScriptableObject<StringVariable>();
+        }
+    }
+}
