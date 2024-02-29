2eec78adb3cc02564565068dfcb4052304621dec
Alex Ruiz <a.ruiz.rabasseda@gmail.com>
2024-02-29 01:53:05 +0100

    feat(variables): add GameObjectVariable
    Signed-off-by: Alex Ruiz <a.ruiz.rabasseda@gmail.com>

---
+++ Editor/Variables/GameObjectVariableMenu.cs
@@ -0,0 +1,15 @@
+﻿using SODD.Editor.Utils;
+using SODD.Variables;
+using UnityEditor;
+
+namespace SODD.Editor.Variables
+{
+    public static class GameObjectVariableMenu
+    {
+        [MenuItem("Tools/" + Framework.Variables.GameObject, priority = Framework.MenuOrders.GameObject)]
+        public static void Create(MenuCommand command)
+        {
+            EditorHelper.CreateScriptableObject<GameObjectVariable>();
+        }
+    }
+}
