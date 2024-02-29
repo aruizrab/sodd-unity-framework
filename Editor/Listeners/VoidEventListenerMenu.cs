20754941241f75460a5cb79bf2b367554652e252
Alex Ruiz <a.ruiz.rabasseda@gmail.com>
2024-02-29 23:40:29 +0100

    feat(listeners): add VoidEventListener
    Signed-off-by: Alex Ruiz <a.ruiz.rabasseda@gmail.com>

---
+++ Editor/Listeners/VoidEventListenerMenu.cs
@@ -0,0 +1,17 @@
+﻿using SODD.Editor.Utils;
+using SODD.Listeners;
+using UnityEditor;
+using UnityEngine;
+
+namespace SODD.Editor.Listeners
+{
+    public static class VoidEventListenerMenu
+    {
+        [MenuItem("GameObject/" + Framework.EventListeners.Void, priority = Framework.MenuOrders.Void)]
+        [MenuItem("Tools/" + Framework.EventListeners.Void, priority = Framework.MenuOrders.Void)]
+        public static void CreateVoidListener(MenuCommand command)
+        {
+            EditorHelper.CreateGameObject<VoidEventListener>(command.context as GameObject);
+        }
+    }
+}
---
+++ Runtime/Listeners/VoidEventListener.cs
@@ -0,0 +1,13 @@
+﻿using SODD.Core;
+using UnityEngine;
+
+namespace SODD.Listeners
+{
+    /// <summary>
+    ///     Represents an event listener for void events.
+    /// </summary>
+    [AddComponentMenu(Framework.EventListeners.VoidEventListener, Framework.MenuOrders.Void)]
+    public class VoidEventListener : EventListener<Void>
+    {
+    }
+}
