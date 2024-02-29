1a6e6bcd1d6ac11ae8a3bc4a10c82659269f79ff
Alex Ruiz <a.ruiz.rabasseda@gmail.com>
2024-02-29 18:46:33 +0100

    feat(collections): add ObjectCollection
    Signed-off-by: Alex Ruiz <a.ruiz.rabasseda@gmail.com>

---
+++ Editor/Collections/ObjectCollectionMenu.cs
@@ -0,0 +1,15 @@
+﻿using SODD.Collections;
+using SODD.Editor.Utils;
+using UnityEditor;
+
+namespace SODD.Editor.Collections
+{
+    public static class ObjectCollectionMenu
+    {
+        [MenuItem("Tools/" + Framework.Collections.Object, priority = Framework.MenuOrders.Object)]
+        public static void Create(MenuCommand command)
+        {
+            EditorHelper.CreateScriptableObject<ObjectCollection>();
+        }
+    }
+}
