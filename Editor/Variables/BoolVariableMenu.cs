4bb16ddcd7746311836f2e2690bbcd6bc65ca0fc
Alex Ruiz <a.ruiz.rabasseda@gmail.com>
2024-02-29 01:09:38 +0100

    feat(variables): add BoolVariable
    Signed-off-by: Alex Ruiz <a.ruiz.rabasseda@gmail.com>

---
+++ Editor/Variables/BoolVariableMenu.cs
@@ -0,0 +1,15 @@
+﻿using SODD.Editor.Utils;
+using SODD.Variables;
+using UnityEditor;
+
+namespace SODD.Editor.Variables
+{
+    public static class BoolVariableMenu
+    {
+        [MenuItem("Tools/" + Framework.Variables.Bool, priority = Framework.MenuOrders.Bool)]
+        public static void Create(MenuCommand command)
+        {
+            EditorHelper.CreateScriptableObject<BoolVariable>();
+        }
+    }
+}
