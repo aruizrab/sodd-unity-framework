5cf5f30e4418a794f2e8172865df69c81daccfbc
Alex Ruiz <a.ruiz.rabasseda@gmail.com>
2024-02-29 01:39:25 +0100

    feat(variables): add LayerMaskVariable
    Signed-off-by: Alex Ruiz <a.ruiz.rabasseda@gmail.com>

---
+++ Editor/Variables/LayerMaskVariableMenu.cs
@@ -0,0 +1,15 @@
+﻿using SODD.Editor.Utils;
+using SODD.Variables;
+using UnityEditor;
+
+namespace SODD.Editor.Variables
+{
+    public static class LayerMaskVariableMenu
+    {
+        [MenuItem("Tools/" + Framework.Variables.LayerMask, priority = Framework.MenuOrders.LayerMask)]
+        public static void Create(MenuCommand command)
+        {
+            EditorHelper.CreateScriptableObject<StringVariable>();
+        }
+    }
+}
