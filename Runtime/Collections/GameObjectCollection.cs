a4fb546bdaea838dbd2be4b4eeb0d98c6cf82895
Alex Ruiz <a.ruiz.rabasseda@gmail.com>
2024-02-29 18:38:55 +0100

    feat(collections): add GameObjectCollection
    Signed-off-by: Alex Ruiz <a.ruiz.rabasseda@gmail.com>

---
+++ Runtime/Collections/GameObjectCollection.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace SODD.Collections
+{
+    /// <summary>
+    ///     Represents a collection that stores GameObjects.
+    /// </summary>
+    /// <seealso cref="Collection{T}" />
+    [CreateAssetMenu(menuName = Framework.Collections.GameObject, fileName = nameof(GameObjectCollection),
+        order = Framework.MenuOrders.GameObject)]
+    public sealed class GameObjectCollection : Collection<GameObject>
+    {
+    }
+}
