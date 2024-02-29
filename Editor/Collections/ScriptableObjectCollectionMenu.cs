6c5abc2bc14cb942e35914f63c91c78aeab3e896
Alex Ruiz <a.ruiz.rabasseda@gmail.com>
2024-02-29 18:40:35 +0100

    feat(collections): add ScriptableObjectCollection
    Signed-off-by: Alex Ruiz <a.ruiz.rabasseda@gmail.com>

---
+++ Editor/Collections/ScriptableObjectCollectionMenu.cs
@@ -0,0 +1,15 @@
+﻿using SODD.Collections;
+using SODD.Editor.Utils;
+using UnityEditor;
+
+namespace SODD.Editor.Collections
+{
+    public static class ScriptableObjectCollectionMenu
+    {
+        [MenuItem("Tools/" + Framework.Collections.ScriptableObject, priority = Framework.MenuOrders.ScriptableObject)]
+        public static void Create(MenuCommand command)
+        {
+            EditorHelper.CreateScriptableObject<ScriptableObjectCollection>();
+        }
+    }
+}
