f6ce12aec6e888820272e9314ed4c73158ddef8c
Alex Ruiz <a.ruiz.rabasseda@gmail.com>
2024-03-02 13:18:05 +0100

    feat(input): add Vector3InputActionHandler
    Signed-off-by: Alex Ruiz <a.ruiz.rabasseda@gmail.com>

---
+++ Editor/Input/ActionHandlers/Vector3InputActionHandlerMenu.cs
@@ -0,0 +1,26 @@
+﻿using SODD.Editor.Utils;
+using SODD.Input.ActionHandlers;
+using UnityEditor;
+
+namespace SODD.Editor.Input.ActionHandlers
+{
+    /// <summary>
+    ///     A ScriptableObject responsible for handling input actions that produce a <see cref="Vector3" /> value.
+    /// </summary>
+    /// <remarks>
+    ///     <para>
+    ///         The `Vector3InputActionHandler` specializes in processing input actions associated with three-dimensional
+    ///         movement or directional input, such as joystick movements in 3D space, or more complex scenarios where
+    ///         an input generates a three-dimensional vector. It is particularly useful in 3D games for camera control,
+    ///         character movement in three axes, or any gameplay mechanism requiring three-dimensional directional input.
+    ///     </para>
+    /// </remarks>
+    public static class Vector3InputActionHandlerMenu
+    {
+        [MenuItem("Tools/" + Framework.Input.ActionHandlers.Vector3, priority = Framework.MenuOrders.Vector3)]
+        public static void Create(MenuCommand command)
+        {
+            EditorHelper.CreateScriptableObject<Vector3InputActionHandler>();
+        }
+    }
+}
---
+++ Runtime/Input/ActionHandlers/Vector3InputActionHandler.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace SODD.Input.ActionHandlers
+{
+    [CreateAssetMenu(menuName = Framework.Input.ActionHandlers.Vector3, fileName = nameof(Vector3InputActionHandler),
+        order = Framework.MenuOrders.Vector3)]
+    public sealed class Vector3InputActionHandler : InputActionHandler<Vector3>
+    {
+    }
+}
