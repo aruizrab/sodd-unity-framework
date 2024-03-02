4502560305599fbd99c83112e73727316f1d051e
Alex Ruiz <a.ruiz.rabasseda@gmail.com>
2024-03-02 12:57:30 +0100

    feat(input): add Vector2InputActionHandler
    Signed-off-by: Alex Ruiz <a.ruiz.rabasseda@gmail.com>

---
+++ Editor/Input/ActionHandlers/Vector2InputActionHandlerMenu.cs
@@ -0,0 +1,15 @@
+﻿using SODD.Editor.Utils;
+using SODD.Input.ActionHandlers;
+using UnityEditor;
+
+namespace SODD.Editor.Input.ActionHandlers
+{
+    public static class Vector2InputActionHandlerMenu
+    {
+        [MenuItem("Tools/" + Framework.Input.ActionHandlers.Vector2, priority = Framework.MenuOrders.Vector2)]
+        public static void Create(MenuCommand command)
+        {
+            EditorHelper.CreateScriptableObject<Vector2InputActionHandler>();
+        }
+    }
+}
