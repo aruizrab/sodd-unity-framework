d5f8d7229046e31820b731d89593fb304f189fb9
Alex Ruiz <a.ruiz.rabasseda@gmail.com>
2024-03-02 12:07:39 +0100

    feat(input): add BoolInputActionHandler
    Signed-off-by: Alex Ruiz <a.ruiz.rabasseda@gmail.com>

---
+++ Editor/Input/ActionHandlers/BoolInputActionHandlerMenu.cs
@@ -0,0 +1,15 @@
+﻿using SODD.Editor.Utils;
+using SODD.Input.ActionHandlers;
+using UnityEditor;
+
+namespace SODD.Editor.Input.ActionHandlers
+{
+    public static class BoolInputActionHandlerMenu
+    {
+        [MenuItem("Tools/" + Framework.Input.ActionHandlers.Bool, priority = Framework.MenuOrders.Bool)]
+        public static void Create(MenuCommand command)
+        {
+            EditorHelper.CreateScriptableObject<BoolInputActionHandler>();
+        }
+    }
+}
---
+++ Runtime/Input/ActionHandlers/BoolInputActionHandler.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace SODD.Input.ActionHandlers
+{
+    /// <summary>
+    ///     A ScriptableObject that handles boolean input actions.
+    /// </summary>
+    /// <remarks>
+    ///     <para>
+    ///         The <c>BoolInputActionHandler</c> is designed to process boolean input actions, providing a structured and
+    ///         reusable way to react to button press or toggle events within Unity's Input System. This concrete
+    ///         implementation of  <see cref="InputActionHandler{T}" /> offers specific overrides for the action started,
+    ///         performed, and canceled
+    ///         events, translating input action data to boolean values.
+    ///     </para>
+    /// </remarks>
+    [CreateAssetMenu(menuName = Framework.Input.ActionHandlers.Bool, fileName = nameof(BoolInputActionHandler),
+        order = Framework.MenuOrders.Bool)]
+    public sealed class BoolInputActionHandler : InputActionHandler<bool>
+    {
+        /// <summary>
+        ///     Invoked when the boolean input action starts.
+        /// </summary>
+        /// <param name="context">The callback context from the input system.</param>
+        protected override void OnActionStarted(InputAction.CallbackContext context)
+        {
+            if (onActionStarted) onActionStarted.Invoke(context.ReadValueAsButton());
+        }
+
+        /// <summary>
+        ///     Invoked when the boolean input action is performed.
+        /// </summary>
+        /// <param name="context">The callback context from the input system.</param>
+        protected override void OnActionPerformed(InputAction.CallbackContext context)
+        {
+            if (onActionPerformed) onActionPerformed.Invoke(context.ReadValueAsButton());
+        }
+
+        /// <summary>
+        ///     Invoked when the boolean input action is canceled.
+        /// </summary>
+        /// <param name="context">The callback context from the input system.</param>
+        protected override void OnActionCanceled(InputAction.CallbackContext context)
+        {
+            if (onActionCanceled) onActionCanceled.Invoke(context.ReadValueAsButton());
+        }
+    }
+}
