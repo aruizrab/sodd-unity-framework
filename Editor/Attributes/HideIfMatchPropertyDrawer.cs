d0601c8bffa9c6ecd85b99aa1a8203c7bd48eece
Alex Ruiz <a.ruiz.rabasseda@gmail.com>
2024-05-09 23:43:18 +0200

    feat(attributes): add HideIfMatch attribute
    Signed-off-by: Alex Ruiz <a.ruiz.rabasseda@gmail.com>

---
+++ Editor/Attributes/HideIfMatchPropertyDrawer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using SODD.Attributes;
+using SODD.Editor.Utils;
+using UnityEditor;
+using UnityEngine;
+
+namespace SODD.Editor.Attributes
+{
+    [CustomPropertyDrawer(typeof(HideIfMatchAttribute))]
+    public class HideIfMatchPropertyDrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            if (HideProperty(property)) return;
+            EditorGUI.PropertyField(position, property, label);
+        }
+
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            return HideProperty(property) ? 0f : base.GetPropertyHeight(property, label);
+        }
+
+        private bool HideProperty(SerializedProperty property)
+        {
+            try
+            {
+                var hideIfMatchAttribute = (HideIfMatchAttribute) attribute;
+                var value = EditorHelper.GetValue(property, hideIfMatchAttribute.Name);
+                return hideIfMatchAttribute.Values.Aggregate(false, (current, val) => current || Equals(value, val));
+            }
+            catch (Exception e)
+            {
+                Debug.LogError(e.Message);
+                return false;
+            }
+        }
+    }
+}
