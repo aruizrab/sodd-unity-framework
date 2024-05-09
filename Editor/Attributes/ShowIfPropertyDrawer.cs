2a025a792a7dc1b06ad9a3e4fa9a606c0790e3d3
Alex Ruiz <a.ruiz.rabasseda@gmail.com>
2024-05-09 23:57:27 +0200

    feat(attributes): add ShowIf attribute
    Signed-off-by: Alex Ruiz <a.ruiz.rabasseda@gmail.com>

---
+++ Editor/Attributes/ShowIfPropertyDrawer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using SODD.Attributes;
+using SODD.Core;
+using SODD.Editor.Utils;
+using UnityEditor;
+using UnityEngine;
+
+namespace SODD.Editor.Attributes
+{
+    [CustomPropertyDrawer(typeof(ShowIfAttribute))]
+    public class ShowIfPropertyDrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            if (!ShowProperty(property)) return;
+            EditorGUI.PropertyField(position, property, label);
+        }
+
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            return ShowProperty(property) ? base.GetPropertyHeight(property, label) : 0f;
+        }
+
+        private bool ShowProperty(SerializedProperty property)
+        {
+            try
+            {
+                var showIfAttribute = (ShowIfAttribute)attribute;
+                var value = EditorHelper.GetValue(property, showIfAttribute.Name);
+                return showIfAttribute.Comparison.Evaluate(showIfAttribute.Value, value);
+            }
+            catch (Exception e)
+            {
+                Debug.LogError(e.Message);
+                return true;
+            }
+        }
+    }
+}
