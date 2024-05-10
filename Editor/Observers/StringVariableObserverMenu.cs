7cfb246023381d143f360749b79b90abbb577cd7
Alex Ruiz <a.ruiz.rabasseda@gmail.com>
2024-05-10 17:50:02 +0200

    feat(observers): add string variable observer
    Signed-off-by: Alex Ruiz <a.ruiz.rabasseda@gmail.com>

---
+++ Editor/Observers/StringVariableObserverMenu.cs
@@ -0,0 +1,17 @@
+﻿using SODD.Editor.Utils;
+using SODD.Observers;
+using UnityEditor;
+using UnityEngine;
+
+namespace SODD.Editor.Observers
+{
+    public static class StringVariableObserverMenu
+    {
+        [MenuItem("GameObject/" + Framework.VariableObservers.String, priority = Framework.MenuOrders.String)]
+        [MenuItem("Tools/" + Framework.VariableObservers.String, priority = Framework.MenuOrders.String)]
+        public static void CreateStringListener(MenuCommand command)
+        {
+            EditorHelper.CreateGameObject<StringVariableObserver>(command.context as GameObject);
+        }
+    }
+}
---
+++ Runtime/Observers/StringVariableObserver.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace SODD.Observers
+{
+    /// <summary>
+    ///     Represents a component that observes changes in a <see cref="Variables.StringVariable" />'s value and triggers
+    ///     UnityEvents in response.
+    /// </summary>
+    /// <remarks>
+    ///     This class inherits from the <see cref="VariableObserver{T}" /> where <c>T</c> is a string.
+    ///     It provides a ready-to-use observer component that can be attached to any GameObject to monitor changes in string
+    ///     variables.
+    ///     This observer does not implement additional logic; it utilizes the functionality provided by its base class
+    ///     to link string variable changes directly to Unity events.
+    /// </remarks>
+    [AddComponentMenu(Framework.VariableObservers.StringVariableObserver, Framework.MenuOrders.String)]
+    public class StringVariableObserver : VariableObserver<string>
+    {
+    }
+}
