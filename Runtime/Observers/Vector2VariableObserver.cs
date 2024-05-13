ae0b5802b104ff08a957395f15b4599bf70b9c74
Alex Ruiz <a.ruiz.rabasseda@gmail.com>
2024-05-13 19:16:00 +0200

    feat(observers): add vector2 variable observer
    Signed-off-by: Alex Ruiz <a.ruiz.rabasseda@gmail.com>

---
+++ Runtime/Observers/Vector2VariableObserver.cs
@@ -0,0 +1,23 @@
+﻿using SODD.Attributes;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace SODD.Observers
+{
+    /// <summary>
+    ///     Represents a component that observes changes in a <see cref="Variables.Vector2Variable" />'s value and triggers
+    ///     UnityEvents in response.
+    /// </summary>
+    [AddComponentMenu(Framework.VariableObservers.Vector2VariableObserver, Framework.MenuOrders.Vector2)]
+    public class Vector2VariableObserver : VariableObserver<Vector2>
+    {
+        [SerializeField] [Collapsible("Get Value As String")]
+        public UnityEvent<string> onValueChangedAsString;
+
+        protected override void OnVariableValueChanged(Vector2 value)
+        {
+            base.OnVariableValueChanged(value);
+            onValueChangedAsString?.Invoke(value.ToString());
+        }
+    }
+}
