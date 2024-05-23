5d7b2f1a2b7ed6ac62af00d96c8d1c402f3a93a9
Alex Ruiz <a.ruiz.rabasseda@gmail.com>
2024-05-23 21:44:00 +0200

    feat(ai): add 2D collision detector
    Signed-off-by: Alex Ruiz <a.ruiz.rabasseda@gmail.com>

---
+++ Runtime/AI/CollisionDetector2D.cs
@@ -0,0 +1,46 @@
+﻿using SODD.Core;
+using SODD.Variables;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace SODD.AI
+{
+    public sealed class CollisionDetector2D: MonoBehaviour
+    {
+        [SerializeField] private ValueReference<LayerMask> targetLayers;
+        [SerializeField] private CollisionDetectionStrategy detectionStrategy;
+
+        public UnityEvent<GameObject> onCollisionEnter;
+        public UnityEvent<GameObject> onCollisionExit;
+
+        private void OnCollisionEnter2D(Collision2D other)
+        {
+            if (detectionStrategy.HasFlag(CollisionDetectionStrategy.Colliders)) OnEnter(other.gameObject);
+        }
+
+        private void OnCollisionExit2D(Collision2D other)
+        {
+            if (detectionStrategy.HasFlag(CollisionDetectionStrategy.Colliders)) OnExit(other.gameObject);
+        }
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (detectionStrategy.HasFlag(CollisionDetectionStrategy.TriggerColliders)) OnEnter(other.gameObject);
+        }
+
+        private void OnTriggerExit2D(Collider2D other)
+        {
+            if (detectionStrategy.HasFlag(CollisionDetectionStrategy.TriggerColliders)) OnExit(other.gameObject);
+        }
+
+        private void OnEnter(GameObject obj)
+        {
+            if (obj.IsInLayerMask(targetLayers.Value)) onCollisionEnter?.Invoke(obj);
+        }
+
+        private void OnExit(GameObject obj)
+        {
+            if (obj.IsInLayerMask(targetLayers.Value)) onCollisionExit?.Invoke(obj);
+        }
+    }
+}
