b926c21de5b9040a8c70026364ed5bf9c9fe8a34
Alex Ruiz <a.ruiz.rabasseda@gmail.com>
2024-05-23 21:36:06 +0200

    feat(ai): add state machine
    Signed-off-by: Alex Ruiz <a.ruiz.rabasseda@gmail.com>

---
+++ Editor/AI/StateMachineEditor.cs
@@ -0,0 +1,44 @@
+﻿using System.Linq;
+using SODD.AI;
+using UnityEditor;
+using UnityEngine;
+
+namespace SODD.Editor.AI
+{
+    [CustomEditor(typeof(StateMachine))]
+    public class StateMachineEditor : UnityEditor.Editor
+    {
+        private SerializedProperty statesProperty;
+
+        private void OnEnable()
+        {
+            statesProperty = serializedObject.FindProperty("states");
+        }
+
+        public override void OnInspectorGUI()
+        {
+            DrawDefaultInspector();
+
+            var stateMachine = (StateMachine)target;
+
+            if (GUILayout.Button("Add all children states")) AddChildStates(stateMachine);
+        }
+
+        private void AddChildStates(StateMachine stateMachine)
+        {
+            var childStates = stateMachine.GetComponentsInChildren<State>().ToList();
+
+            serializedObject.Update();
+            statesProperty.ClearArray();
+            
+            for (var i = 0; i < childStates.Count; i++)
+            {
+                statesProperty.InsertArrayElementAtIndex(i);
+                statesProperty.GetArrayElementAtIndex(i).objectReferenceValue = childStates[i];
+            }
+
+            serializedObject.ApplyModifiedProperties();
+            EditorUtility.SetDirty(stateMachine);
+        }
+    }
+}
---
+++ Runtime/AI/State.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace SODD.AI
+{
+    [AddComponentMenu(Framework.State, Framework.MenuOrders.State)]
+    public sealed class State : MonoBehaviour
+    {
+        [SerializeField] private UnityEvent onEnter;
+        [SerializeField] private UnityEvent onExit;
+
+        public void Enter()
+        {
+            gameObject.SetActive(true);
+            onEnter?.Invoke();
+        }
+
+        public void Exit()
+        {
+            gameObject.SetActive(false);
+            onExit?.Invoke();
+        }
+    }
+}
