5d42ecdf0ee4d9a487a2d16994a6c112d16be0cc
Alex Ruiz <a.ruiz.rabasseda@gmail.com>
2024-05-25 21:40:59 +0200

    feat(repositories): add variable repository debugging
    Signed-off-by: Alex Ruiz <a.ruiz.rabasseda@gmail.com>

--- Runtime/Repositories/VariableRepository.cs
+++ Runtime/Repositories/VariableRepository.cs
@@ -1,8 +1,13 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Text;
+using SODD.Core;
 using SODD.Data;
 using SODD.Variables;
 using UnityEngine;
+using JsonUtility = UnityEngine.JsonUtility;
+using Logger = SODD.Core.Logger;
 using Object = UnityEngine.Object;
 
 namespace SODD.Repositories
@@ -22,6 +27,11 @@ namespace SODD.Repositories
         [SerializeField] private string filename;
         [SerializeField] private bool loadOnEnable;
         [SerializeField] private bool saveOnDisable;
+#if UNITY_EDITOR
+        [SerializeField]
+        [Tooltip("Enable this setting to log in the console whenever the repository saves or loads variable data.")]
+        public bool debug;
+#endif
         [SerializeField] private List<Object> variables;
 
         private void OnEnable()
@@ -45,6 +55,19 @@ namespace SODD.Repositories
 
             data.Store(list);
             repository.Save(data);
+#if UNITY_EDITOR
+            if (!debug) return;
+            if (list.IsEmpty())
+            {
+                Logger.LogAsset(this, "Variable list is empty, no data will be saved.");
+                return;
+            }
+            Logger.LogAsset(this,
+                $"{list.Count} variable{(list.Count > 1 ? "s" : "")} ha{(list.Count > 1 ? "ve" : "s")} been saved." + Environment.NewLine +
+                $"Destination file: {Application.persistentDataPath}/{filename}.bin" + Environment.NewLine +
+                "Data: [" + Environment.NewLine +
+                string.Join("," + Environment.NewLine, list.Select(item => JsonUtility.ToJson(item, true))) + "]");
+#endif
         }
 
         /// <summary>
@@ -54,12 +77,34 @@ namespace SODD.Repositories
         {
             var repository = new BinaryFileRepository<VariableStore>(filename);
 
-            if (!repository.Exists()) return;
+            if (!repository.Exists())
+            {
+#if UNITY_EDITOR
+                if (debug)
+                    Logger.LogAsset(this,
+                        "Variable data file not found, no data will be loaded.\n" +
+                        $"Source file: {Application.persistentDataPath}/{filename}.bin");
+#endif
+                return;
+            }
 
             var list = variables.OfType<IVariable>().ToList();
             var data = repository.Load();
 
             data.Restore(list);
+#if UNITY_EDITOR
+            if (!debug) return;
+            if (list.IsEmpty())
+            {
+                Logger.LogAsset(this, "Variable list is empty, no data will be loaded.");
+                return;
+            }
+            Logger.LogAsset(this,
+                $"{list.Count} variable{(list.Count > 1 ? "s" : "")} ha{(list.Count > 1 ? "ve" : "s")} been loaded." + Environment.NewLine +
+                $"Destination file: {Application.persistentDataPath}/{filename}.bin" + Environment.NewLine +
+                "Data: [" + Environment.NewLine +
+                string.Join("," + Environment.NewLine, list.Select(item => JsonUtility.ToJson(item, true))) + "]");
+#endif
         }
 
         /// <summary>
@@ -67,7 +112,27 @@ namespace SODD.Repositories
         /// </summary>
         public void Delete()
         {
-            new BinaryFileRepository<VariableStore>(filename).Delete();
+            var repository = new BinaryFileRepository<VariableStore>(filename);
+            repository.Delete();
+#if UNITY_EDITOR
+            if (!debug) return;
+            Logger.LogAsset(this,
+                "Variable data file has been deleted.\n" +
+                $"Deleted file: {Application.persistentDataPath}/{filename}.bin");
+#endif
         }
+        
+#if UNITY_EDITOR
+        [Serializable]
+        internal class VariableList
+        {
+            public List<IVariable> Variables;
+
+            public VariableList(List<IVariable> variables)
+            {
+                Variables = variables;
+            }
+        }
+#endif
     }
 }
